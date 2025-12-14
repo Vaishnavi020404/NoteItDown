6067514f9b5518b1136b645ca5791fdedca99564
Vaishnavi Pandey <139903857+Vaishnavi020404@users.noreply.github.com>
2025-12-14 18:11:43 +0530

    prepared for deployment

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path"
 
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
@@ -11,12 +12,15 @@ dotenv.config();
 
 console.log(process.env.MONGO_URI);
 const app = express();
+const __dirname = path.resolve();
 
+if(process.env.NODE_ENV!=="production"){
 app.use(
   cors({
     origin: "http://localhost:5173", 
   })
 );
+}
 app.use(express.json()); 
 
 app.use(rateLimiter);
@@ -27,6 +31,14 @@ app.use((req,res,next)=>{
     next();
 })
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV==="production"){
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
+});
+}
 
 connectDB().then(()=>{
     app.listen(5001, () => {
